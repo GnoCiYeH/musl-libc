95a621f0421228ece7332ab7c355df514f88267a
GnoCiYeH <heyicong@dragonos.org>
2023-10-21 19:23:18 +0800

    update

--- src/thread/x86_64/__set_thread_area.s
+++ src/thread/x86_64/__set_thread_area.s
@@ -4,7 +4,7 @@
 .hidden __set_thread_area
 .type __set_thread_area,@function
 __set_thread_area:
-	mov %rdi,%rsi           /* shift for syscall */
+	mov %rdi,%r9           /* shift for syscall */
 	movq $0x1002,%r8       /* SET_FS register */
 	movq $158,%rax          /* set fs segment to */
 	int $0x80                 /* arch_prctl(SET_FS, arg)*/
