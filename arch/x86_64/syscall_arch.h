6164f2b5b4a8355f985e89ce51008da6b9a78098
GnoCiYeH <heyicong@dragonos.org>
2023-10-24 22:35:16 +0800

    update

--- arch/x86_64/syscall_arch.h
+++ arch/x86_64/syscall_arch.h
@@ -3,13 +3,13 @@
 
 static __inline long __syscall0(long n) {
   unsigned long ret;
-  __asm__ __volatile__("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+  __asm__ __volatile__("int $0x80" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
   return ret;
 }
 
 static __inline long __syscall1(long n, long a1) {
   unsigned long ret;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1)
                        : "rcx", "r11", "memory");
@@ -18,7 +18,7 @@ static __inline long __syscall1(long n, long a1) {
 
 static __inline long __syscall2(long n, long a1, long a2) {
   unsigned long ret;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1), "S"(a2)
                        : "rcx", "r11", "memory");
@@ -27,7 +27,7 @@ static __inline long __syscall2(long n, long a1, long a2) {
 
 static __inline long __syscall3(long n, long a1, long a2, long a3) {
   unsigned long ret;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1), "S"(a2), "d"(a3)
                        : "rcx", "r11", "memory");
@@ -37,7 +37,7 @@ static __inline long __syscall3(long n, long a1, long a2, long a3) {
 static __inline long __syscall4(long n, long a1, long a2, long a3, long a4) {
   unsigned long ret;
   register long r10 __asm__("r10") = a4;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10)
                        : "rcx", "r11", "memory");
@@ -49,7 +49,7 @@ static __inline long __syscall5(long n, long a1, long a2, long a3, long a4,
   unsigned long ret;
   register long r10 __asm__("r10") = a4;
   register long r8 __asm__("r8") = a5;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8)
                        : "rcx", "r11", "memory");
@@ -62,7 +62,7 @@ static __inline long __syscall6(long n, long a1, long a2, long a3, long a4,
   register long r10 __asm__("r10") = a4;
   register long r8 __asm__("r8") = a5;
   register long r9 __asm__("r9") = a6;
-  __asm__ __volatile__("syscall"
+  __asm__ __volatile__("int $0x80"
                        : "=a"(ret)
                        : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8),
                          "r"(r9)
